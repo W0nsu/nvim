1eb889c70177e80d2fb4aa6238e87225be4316b0
Paweł Wąsowski <pawelwasowski@pawes-mbp.mynet>
2023-11-01 09:16:26 +0100

    Plugins organized + harpoon config changed

--- lua/wonsu/plugins.lua
+++ lua/wonsu/plugins.lua
@@ -1,56 +1,50 @@
 return {
-  -- Git related plugins
+  -- Git 
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
+  --
 
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
-
-  -- NOTE: This is where your plugins related to LSP can be installed.
-  --  The configuration is done below. Search for lspconfig to find it below.
+  -- LSP 
   {
-    -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
-
-      -- Useful status updates for LSP
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
-
-      -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
     },
   },
+  --
 
+  -- C#
   {
-    -- Autocompletion
+    'omniSharp/omnisharp-vim',
+    dependencies = {
+      'omnisharp_extended'
+    }
+  },
+  --
+
+  -- Autocompletion
+  {
     'hrsh7th/nvim-cmp',
     dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
       'L3MON4D3/LuaSnip',
       'saadparwaiz1/cmp_luasnip',
-
-      -- Adds LSP completion capabilities
       'hrsh7th/cmp-nvim-lsp',
-
-      -- Adds a number of user-friendly snippets
       'rafamadriz/friendly-snippets',
     },
   },
+  --
 
-  -- Useful plugin to show you pending keybinds.
+  -- Pending keybinds 
   { 'folke/which-key.nvim', opts = {} },
+  --
 
+  -- Interface
   'folke/tokyonight.nvim',
-  'theprimeagen/harpoon',
-
   {
-    -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
     opts = {
       options = {
         icons_enabled = false,
@@ -60,32 +54,21 @@ return {
       },
     },
   },
-
   {
-    -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
     main = 'ibl',
     opts = {},
   },
+  --
 
-    -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
-
-  -- Fuzzy Finder (files, lsp, etc)
+  -- Files navigation
   {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-      -- Only load if `make` is available. Make sure you have the system
-      -- requirements installed.
       {
         'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
         build = 'make',
         cond = function()
           return vim.fn.executable 'make' == 1
@@ -93,15 +76,20 @@ return {
       },
     },
   },
+  'theprimeagen/harpoon',
+  --
 
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+  --
+
+  -- Treesitter
   {
-    -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
     build = ':TSUpdate',
   },
-
-  'omniSharp/omnisharp-vim'
+  --
 }
