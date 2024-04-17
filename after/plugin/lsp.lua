2ac3dd7f84dda35c69b7d8118b5f4e60252e990d
Pawel Wasowski <pawel.wasowski@angrynerds.co>
2024-04-17 15:33:57 +0200

    Code navigation changed from vim to telescope

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,16 +7,27 @@ lsp.ensure_installed({
     --[[ 'csharp_ls' ]]
 })
 
+-- Fix Undefined global 'vim'
+--[[ lsp.configure('lua-language-server', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+}) ]]
+
 local on_attach = function(_, bufnr)
 	local opts = {buffer = bufnr, remap = false}
 
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	--[[ vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 	vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
 	vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
 	vim.keymap.set("n", "ca", function() vim.lsp.buf.code_action() end, opts)
 	vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.rename() end, opts)
 	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts) ]]
 end
 
 lsp.on_attach(on_attach)
@@ -27,4 +38,9 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 require('roslyn').setup({
     on_attach = on_attach,
     capabilities = capabilities
+})
+
+vim.diagnostic.config({
+    virtual_text = true,
+    virtual_lines = false
 })
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -21,3 +21,45 @@ vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch curren
 vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+    callback = function(event)
+        local map = function(keys, func, desc)
+            vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+        end
+
+        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
+        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
+        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+
+        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+
+        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+
+        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+        map('K', vim.lsp.buf.hover, 'Hover Documentation')
+
+        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        if client and client.server_capabilities.documentHighlightProvider then
+            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+                buffer = event.buf,
+                callback = vim.lsp.buf.document_highlight,
+            })
+
+            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+                buffer = event.buf,
+                callback = vim.lsp.buf.clear_references,
+            })
+        end
+    end,
+})
